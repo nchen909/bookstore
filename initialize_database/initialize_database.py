095f0a97c57553fd8b9d312427cef873fb333dc9
yinzhangyue <yinzhangyue@users.noreply.github.com>
2019-12-14 11:47:37 +0800

    新增book表设计

--- initialize_database/initialize_database.py
+++ initialize_database/initialize_database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, ForeignKey, create_engine, PrimaryKeyConstraint, Text, DateTime, Boolean
+from sqlalchemy import Column, String, Integer, ForeignKey, create_engine, PrimaryKeyConstraint, Text, DateTime, Boolean, LargeBinary
 from sqlalchemy.orm import sessionmaker
 
 import psycopg2
@@ -16,32 +16,51 @@ Base = declarative_base()
 # 用户表
 class User(Base):
     __tablename__ = 'usr'
-    user_id = Column(String(64), primary_key=True)
-    password = Column(String(64), nullable=False)
+    user_id = Column(String(128), primary_key=True)
+    password = Column(String(128), nullable=False)
     balance = Column(Integer, nullable=False)
-    token = Column(String(512))
+    token = Column(String(400))
     terminal = Column(String(64))
 
 
 # 商店表（含书本信息）
 class Store(Base):
     __tablename__ = 'store'
-    store_id = Column(String(64), nullable=False)
-    book_id = Column(String(64), nullable=False)
-    book_info = Column(Text, nullable=False)
+    store_id = Column(String(128), nullable=False)
+    book_id = Column(Integer, ForeignKey('book.book_id'), nullable=False)
     stock_level = Column(Integer, nullable=False)
-    price = Column(Integer, nullable=False)
+    price = Column(Integer, nullable=False) # 售价
     __table_args__ = (
         PrimaryKeyConstraint('store_id', 'book_id'),
         {},
     )
 
+class Book(Base):
+    __tablename__ = 'book'
+    book_id = Column(Integer, primary_key=True)
+    title = Column(Text, nullable=False)
+    author = Column(Text)
+    publisher = Column(Text)
+    original_title = Column(Text)
+    translator = Column(Text)
+    pub_year = Column(Text)
+    pages = Column(Integer)
+    original_price = Column(Integer) # 原价
+    currency_unit = Column(String(16))
+    binding = Column(Text)
+    isbn = Column(Text)
+    author_intro = Column(Text)
+    book_intro = Column(Text)
+    content = Column(Text)
+    tags = Column(Text)
+    picture = Column(LargeBinary)
+
 
 # 用户商店关系表
 class User_store(Base):
     __tablename__ = 'user_store'
-    user_id = Column(String(64), ForeignKey('usr.user_id'), nullable=False)
-    store_id = Column(String(64), nullable=False)
+    user_id = Column(String(128), ForeignKey('usr.user_id'), nullable=False)
+    store_id = Column(String(128), nullable=False)
     __table_args__ = (
         PrimaryKeyConstraint('user_id', 'store_id'),
         {},
@@ -52,8 +71,8 @@ class User_store(Base):
 class New_order_pend(Base):
     __tablename__ = 'new_order_pend'
     order_id = Column(String(128), primary_key=True)
-    buyer_id = Column(String(64), ForeignKey('usr.user_id'), nullable=False)
-    seller_id = Column(String(64), ForeignKey('usr.user_id'), nullable=False)
+    buyer_id = Column(String(128), ForeignKey('usr.user_id'), nullable=False)
+    seller_id = Column(String(128), ForeignKey('usr.user_id'), nullable=False)
     price = Column(Integer, nullable=False)
     pt = Column(DateTime, nullable=False)
 
@@ -62,8 +81,8 @@ class New_order_pend(Base):
 class New_order_cancel(Base):
     __tablename__ = 'new_order_cancel'
     order_id = Column(String(128), primary_key=True)
-    buyer_id = Column(String(64), ForeignKey('usr.user_id'), nullable=False)
-    seller_id = Column(String(64), ForeignKey('usr.user_id'), nullable=False)
+    buyer_id = Column(String(128), ForeignKey('usr.user_id'), nullable=False)
+    seller_id = Column(String(128), ForeignKey('usr.user_id'), nullable=False)
     price = Column(Integer, nullable=False)
     pt = Column(DateTime, nullable=False)
 
@@ -71,8 +90,8 @@ class New_order_cancel(Base):
 class New_order_paid(Base):
     __tablename__ = 'new_order_paid'
     order_id = Column(String(128), primary_key=True)
-    buyer_id = Column(String(64), ForeignKey('usr.user_id'), nullable=False)
-    seller_id = Column(String(64), ForeignKey('usr.user_id'), nullable=False)
+    buyer_id = Column(String(128), ForeignKey('usr.user_id'), nullable=False)
+    seller_id = Column(String(128), ForeignKey('usr.user_id'), nullable=False)
     price = Column(Integer, nullable=False)
     pt = Column(DateTime, nullable=False)
     status = Column(Boolean, nullable=False) # 0为已发货，1为已收获
@@ -82,14 +101,13 @@ class New_order_paid(Base):
 class New_order_detail(Base):
     __tablename__ = 'new_order_detail'
     order_id = Column(String(128), nullable=False)
-    book_id = Column(String(64), nullable=False)
+    book_id = Column(Integer, nullable=False)
     count = Column(Integer, nullable=False)
     price = Column(Integer, nullable=False)
     __table_args__ = (
         PrimaryKeyConstraint('order_id', 'book_id'),
         {},
     )
-
 
 def init():
     DBSession = sessionmaker(bind=engine)
@@ -114,17 +132,20 @@ def add_info():
             balance = 500,
             token = '***',
             terminal='Chrome')
+    Book1 = Book(book_id = 0,
+                title='数据结构')
+    Book2 = Book(book_id=1,
+                title='PRML')
+    session.add_all([A, B, Book1, Book2])
     StoreA = Store(store_id = '王掌柜的书店',
-                    book_id = '数据结构',
-                    book_info='本科生必修',
+                    book_id = 0,
                     stock_level=10,
                     price=1000) # 价格单位是分
     StoreB = Store(store_id = '王掌柜的进口书店',
-                    book_id = 'PRML',
-                    book_info='机器学习必读书本',
+                    book_id = 1,
                     stock_level=10,
                     price=10000)
-    session.add_all([A, B, StoreA, StoreB])
+    session.add_all([StoreA, StoreB])
     session.commit()
     A_Store1 = User_store(user_id = '王掌柜',
                         store_id = '王掌柜的书店')
@@ -137,7 +158,7 @@ def add_info():
                             pt = datetime.now(),
                             status = 0)  # 0为已发货，1为已收获
     Order_detailA = New_order_detail(order_id = 'order1',
-                                    book_id = '数据结构',
+                                    book_id = 0,
                                     count = 2,
                                     price = 2000)
     OrderB = New_order_pend(order_id = 'order2',
@@ -146,7 +167,7 @@ def add_info():
                             price = 10000,
                             pt = datetime.now())
     Order_detailB = New_order_detail(order_id = 'order2',
-                                    book_id = 'PRML',
+                                    book_id = 1,
                                     count = 1,
                                     price = 10000)
     session.add_all([
