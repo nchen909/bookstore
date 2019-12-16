9b3928ba2baa2a96937e22bd514e6e39e98e1861
yinzhangyue <yinzhangyue@126.com>
2019-12-16 19:15:20 +0800

    新建表成功

--- initialize_database/initialize_database.py
+++ initialize_database/initialize_database.py
@@ -4,12 +4,15 @@ from sqlalchemy import Column, String, Integer, ForeignKey, create_engine, Prima
 from sqlalchemy.orm import sessionmaker
 
 import psycopg2
-from datetime import datetime,time
+from datetime import datetime, time
 # 连接数据库legend 记得修改这个！！！
 # engine = create_engine('postgresql://postgres:amyamy@localhost:5433/bookstore')
-engine = create_engine('postgresql://postgres:990814@[2001:da8:8005:4056:81e9:7f6c:6d05:fe47]:5432/Bookstore')
+engine = create_engine(
+    'postgresql://postgres:990814@[2001:da8:8005:4056:81e9:7f6c:6d05:fe47]:5432/bookstore'
+)
 
 Base = declarative_base()
+
 
 # String长度可能需要做修改
 # 用户表
@@ -28,11 +31,12 @@ class Store(Base):
     store_id = Column(String(128), nullable=False)
     book_id = Column(Integer, ForeignKey('book.book_id'), nullable=False)
     stock_level = Column(Integer, nullable=False)
-    price = Column(Integer, nullable=False) # 售价
+    price = Column(Integer, nullable=False)  # 售价
     __table_args__ = (
         PrimaryKeyConstraint('store_id', 'book_id'),
         {},
     )
+
 
 class Book(Base):
     __tablename__ = 'book'
@@ -44,7 +48,7 @@ class Book(Base):
     translator = Column(Text)
     pub_year = Column(Text)
     pages = Column(Integer)
-    original_price = Column(Integer) # 原价
+    original_price = Column(Integer)  # 原价
     currency_unit = Column(String(16))
     binding = Column(Text)
     isbn = Column(Text)
@@ -85,6 +89,7 @@ class New_order_cancel(Base):
     price = Column(Integer, nullable=False)
     pt = Column(DateTime, nullable=False)
 
+
 # 已付款订单
 class New_order_paid(Base):
     __tablename__ = 'new_order_paid'
@@ -93,7 +98,7 @@ class New_order_paid(Base):
     seller_id = Column(String(128), ForeignKey('usr.user_id'), nullable=False)
     price = Column(Integer, nullable=False)
     pt = Column(DateTime, nullable=False)
-    status = Column(Integer, nullable=False) # 0为待发货，1为已发货，2为已收货
+    status = Column(Integer, nullable=False)  # 0为待发货，1为已发货，2为已收货
 
 
 # 订单中的书本信息
@@ -108,6 +113,7 @@ class New_order_detail(Base):
         {},
     )
 
+
 def init():
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
@@ -117,61 +123,53 @@ def init():
     # 关闭session
     session.close()
 
+
 def add_info():
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
     # 提交即保存到数据库A
-    A = User(user_id = '王掌柜',
-            password = '123456',
-            balance = 100,
-            token = '***',
-            terminal = 'Edge')
-    B = User(user_id = '小明',
-            password = '123456',
-            balance = 500,
-            token = '***',
-            terminal='Chrome')
-    Book1 = Book(book_id = 0,
-                title='数据结构')
-    Book2 = Book(book_id=1,
-                title='PRML')
+    A = User(user_id='王掌柜',
+             password='123456',
+             balance=100,
+             token='***',
+             terminal='Edge')
+    B = User(user_id='小明',
+             password='123456',
+             balance=500,
+             token='***',
+             terminal='Chrome')
+    Book1 = Book(book_id=0, title='数据结构')
+    Book2 = Book(book_id=1, title='PRML')
     session.add_all([A, B, Book1, Book2])
-    StoreA = Store(store_id = '王掌柜的书店',
-                    book_id = 0,
-                    stock_level=10,
-                    price=1000) # 价格单位是分
-    StoreB = Store(store_id = '王掌柜的进口书店',
-                    book_id = 1,
-                    stock_level=10,
-                    price=10000)
+    session.commit()
+    StoreA = Store(store_id='王掌柜的书店', book_id=0, stock_level=10,
+                   price=1000)  # 价格单位是分
+    StoreB = Store(store_id='王掌柜的进口书店', book_id=1, stock_level=10, price=10000)
     session.add_all([StoreA, StoreB])
     session.commit()
-    A_Store1 = User_store(user_id = '王掌柜',
-                        store_id = '王掌柜的书店')
-    A_Store2 = User_store(user_id = '王掌柜',
-                        store_id = '王掌柜的进口书店')
-    OrderA = New_order_paid(order_id = 'order1',
-                            buyer_id = '小明',
-                            seller_id = '王掌柜',
+    A_Store1 = User_store(user_id='王掌柜', store_id='王掌柜的书店')
+    A_Store2 = User_store(user_id='王掌柜', store_id='王掌柜的进口书店')
+    OrderA = New_order_paid(order_id='order1',
+                            buyer_id='小明',
+                            seller_id='王掌柜',
                             price=2000,
-                            pt = datetime.now(),
-                            status = 0)  # 0为已发货，1为已收获
-    Order_detailA = New_order_detail(order_id = 'order1',
-                                    book_id = 0,
-                                    count = 2,
-                                    price = 2000)
-    OrderB = New_order_pend(order_id = 'order2',
-                            buyer_id = '小明',
-                            seller_id = '王掌柜',
-                            price = 10000,
-                            pt = datetime.now())
-    Order_detailB = New_order_detail(order_id = 'order2',
-                                    book_id = 1,
-                                    count = 1,
-                                    price = 10000)
-    session.add_all([
-        A_Store1, A_Store2, OrderA, Order_detailA, OrderB, Order_detailB
-    ])
+                            pt=datetime.now(),
+                            status=0)  # 0为已发货，1为已收获
+    Order_detailA = New_order_detail(order_id='order1',
+                                     book_id=0,
+                                     count=2,
+                                     price=2000)
+    OrderB = New_order_pend(order_id='order2',
+                            buyer_id='小明',
+                            seller_id='王掌柜',
+                            price=10000,
+                            pt=datetime.now())
+    Order_detailB = New_order_detail(order_id='order2',
+                                     book_id=1,
+                                     count=1,
+                                     price=10000)
+    session.add_all(
+        [A_Store1, A_Store2, OrderA, Order_detailA, OrderB, Order_detailB])
     session.commit()
     # 关闭session
     session.close()
