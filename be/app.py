92672e638f81d7ffa55371afd8c1f8e39dd28348
1012598167 <chennuo909@163.com>
2019-12-18 22:48:04 +0800

    tochangebuyer

--- be/app.py
+++ be/app.py
@@ -1,11 +1,20 @@
 
 import sys
 import os
+# import schedule
+# import time
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
 
 from be import serve
 
+# def job():
+#     print("I'm working...")
 if __name__ == "__main__":
     serve.be_run()
+
+    # schedule.every().second.do(job)
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)
