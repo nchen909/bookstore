fdb4e094d6ee2a51f72f431a576deab0540a480f
1012598167 <chennuo909@163.com>
2019-12-26 20:36:37 +0800

    1

--- script/test.sh
+++ script/test.sh
@@ -1,4 +1,4 @@
- #!/bin/sh
+#!/bin/sh
 export PATHONPATH=`pwd`
 #coverage run --timid --branch --source fe,be --concurrency=thread -m pytest -v  -k fe/test/test_new_order.py --ignore=fe/data
 #coverage run --timid --branch --source fe,be --concurrency=thread -m pytest -v  -k fe/test/test_search.py --ignore=fe/data
