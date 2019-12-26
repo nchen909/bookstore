c7e728fc113e27850e42b551d7a16682d3fb041f
sherlockeded <wangziyueamy@sina.com>
2019-12-26 21:36:11 +0800

    fix checkbook bug

--- initialize_database/initialize_search_database.py
+++ initialize_database/initialize_search_database.py
@@ -16,8 +16,7 @@ import datetime
 # 连接数据库legend 记得修改这个！！！
 #engine = create_engine('postgresql://postgres:amyamy@localhost:5433/bookstore')
 engine = create_engine(
-    'postgresql://postgres:990814@[2001:da8:8005:4056:81e9:7f6c:6d05:fe47]:5432/bookstore'
-)
+    'postgresql://postgres:990814@[2001:da8:8005:4056:81e9:7f6c:6d05:fe47]:5432/bookstore')
 
 Base = declarative_base()
 
