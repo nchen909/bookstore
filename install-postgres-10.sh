a2330c7c9937f93bd51295a72437128f6df5ace0
1012598167 <chennuo909@163.com>
2020-12-20 02:04:23 +0800

    update sh with create database

--- install-postgres-10.sh
+++ install-postgres-10.sh
@@ -14,7 +14,9 @@ sudo cp /etc/postgresql/{9.6,10}/main/pg_hba.conf
 
 echo "Restarting Postgres 10"
 sudo service postgresql restart
-sudo find / -name 'postmaster.pid'  2>&1 | grep -v "Permission denied"
-sudo rm /var/ramfs/postgresql/9.6/main/postmaster.pid
+#sudo find / -name 'postmaster.pid'  2>&1 | grep -v "Permission denied"
+#sudo rm /var/ramfs/postgresql/9.6/main/postmaster.pid
+sudo pg_ctl -D /var/ramfs/postgresql/9.6/main -l /var/ramfs/postgresql/9.6/main/server.log start
+sudo cat /var/ramfs/postgresql/9.6/main/server.log
 sudo psql -c 'CREATE ROLE travis SUPERUSER LOGIN CREATEDB;' -U postgres
 sudo psql -c 'create database bookstore;' -U postgres
