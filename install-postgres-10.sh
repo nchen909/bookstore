14c367087275963ff07e427418474118a879277f
1012598167 <chennuo909@163.com>
2020-12-20 01:52:09 +0800

    update sh with create database

--- install-postgres-10.sh
+++ install-postgres-10.sh
@@ -5,8 +5,8 @@ set -ex
 echo "Installing Postgres 10"
 sudo find / -name 'postmaster.pid'  2>&1 | grep -v "Permission denied"
 sudo service postgresql stop
-sudo find / -name 'postmaster.pid'  2>&1 | grep -v "Permission denied"
-#sudo rm /var/lib/postgresql/{9.6,10}/main/postmaster.pid
+#sudo find / -name 'postmaster.pid'  2>&1 | grep -v "Permission denied"
+sudo rm /var/ramfs/postgresql/{9.6,10}/main/postmaster.pid
 sudo apt-get remove -q 'postgresql-*'
 sudo apt-get update -q
 sudo apt-get install -q postgresql-10 postgresql-client-10
@@ -14,8 +14,6 @@ sudo cp /etc/postgresql/{9.6,10}/main/pg_hba.conf
 
 echo "Restarting Postgres 10"
 sudo service postgresql restart
-sudo sleep 5
-
 
 sudo psql -c 'CREATE ROLE travis SUPERUSER LOGIN CREATEDB;' -U postgres
 sudo psql -c 'create database bookstore;' -U postgres
