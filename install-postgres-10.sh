8cafd52b2ea595c0ce111a153b17d66246925b3b
1012598167 <chennuo909@163.com>
2020-12-20 01:34:55 +0800

    update sh with create database

--- install-postgres-10.sh
+++ install-postgres-10.sh
@@ -4,6 +4,7 @@ set -ex
 
 echo "Installing Postgres 10"
 sudo service postgresql stop
+sudo rm /usr/local/var/postgres/postmaster.pid
 sudo apt-get remove -q 'postgresql-*'
 sudo apt-get update -q
 sudo apt-get install -q postgresql-10 postgresql-client-10
@@ -11,8 +12,8 @@ sudo cp /etc/postgresql/{9.6,10}/main/pg_hba.conf
 
 echo "Restarting Postgres 10"
 sudo service postgresql restart
-
+sudo sleep 5
 sudo cat /usr/local/var/postgres/server.log
-sudo rm /usr/local/var/postgres/postmaster.pid
+
 sudo psql -c 'CREATE ROLE travis SUPERUSER LOGIN CREATEDB;' -U postgres
 sudo psql -c 'create database bookstore;' -U postgres
