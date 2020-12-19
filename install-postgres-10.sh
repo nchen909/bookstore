0307a9fa1f93ce18ae87c74eb82096bd82c9870e
1012598167 <chennuo909@163.com>
2020-12-20 01:54:16 +0800

    update sh with create database

--- install-postgres-10.sh
+++ install-postgres-10.sh
@@ -6,7 +6,7 @@ echo "Installing Postgres 10"
 sudo find / -name 'postmaster.pid'  2>&1 | grep -v "Permission denied"
 sudo service postgresql stop
 #sudo find / -name 'postmaster.pid'  2>&1 | grep -v "Permission denied"
-sudo rm /var/ramfs/postgresql/{9.6,10}/main/postmaster.pid
+sudo rm /var/ramfs/postgresql/9.6/main/postmaster.pid
 sudo apt-get remove -q 'postgresql-*'
 sudo apt-get update -q
 sudo apt-get install -q postgresql-10 postgresql-client-10
